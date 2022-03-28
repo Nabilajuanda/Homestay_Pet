f366d5bb5d913352c3960517c4bdb674dce35a85
Nabilajuanda <nabilajuanda@student.telkomuniversity.ac.id>
2022-03-28 20:51:33 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if4056/homestaypet/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if4056.homestaypet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
