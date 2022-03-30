49ae4ffd0833eda4ace50e70250071e3f9ad12f0
Nabilajuanda <nabilajuanda@student.telkomuniversity.ac.id>
2022-03-30 21:44:32 +0700

    Menampilkan data yang sudah dipilih (3)

--- app/src/main/java/org/d3if4056/homestaypet/MainActivity.kt
+++ app/src/main/java/org/d3if4056/homestaypet/MainActivity.kt
@@ -27,9 +27,21 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        // Input berapa lama menginap
+        val jumlahHari = binding.berapaHariNumber.text.toString()
+        if (TextUtils.isEmpty(jumlahHari)) {
+            Toast.makeText(this, R.string.hari_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // Check radio button kosong atau tidak
         val selectedId = binding.radioGroup.checkedRadioButtonId
         if (selectedId == -1) {
             Toast.makeText(this, R.string.spesies_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        if (selectedId == -1) {
+            Toast.makeText(this, R.string.pilihanMakan_invalid, Toast.LENGTH_LONG).show()
             return
         }
 
@@ -40,19 +52,49 @@ class MainActivity : AppCompatActivity() {
         val pilihan = getPilihan(spesies, isCat, isDog)
         binding.dataSpesiesTextView.text = getString(R.string.dataSpesies, pilihan)
 
-        // Input nama hewan peliharaan
+        // Check radio button pilihan makanan
+        val isAlone = selectedId == R.id.bawaSendirButton
+        val isTogether = selectedId == R.id.pengurusButton
+        val data = "Bawa sendiri"
+        val hasilData = getData(data, isAlone, isTogether)
+        binding.dataPilihanMakanTextView.text = getString(R.string.dataMakan, hasilData)
+
+        // GetInput nama hewan peliharaan
         val nama: String = namaHewan
         val inputNama = getInputNama(nama)
         binding.dataNamaTextView.text = getString(R.string.dataNama, inputNama)
 
-        // Input berapa lama menginap
-
-
+        // getInput berapa lama menginap
+        val hari: String = jumlahHari
+        val lamaMenginap = getLamaMenginap(hari)
+        binding.dataHariTextView.text = getString(R.string.dataHari, lamaMenginap)
     }
 
     private fun getInputNama(inputNama: String): String {
         println(inputNama)
         return inputNama
+    }
+
+    private fun getLamaMenginap(jumlahHari: String): String {
+        println(jumlahHari)
+        return jumlahHari
+    }
+
+    private fun getData(hasilData: String, isAlone: Boolean, isTogether: Boolean): String {
+        val stringRes = if(isAlone) {
+            when {
+                hasilData == "Bawa sendiri" -> R.string.pilihan_Makan1
+                else -> R.string.pilihan_Makan2
+            }
+        } else if (isTogether) {
+            when {
+                hasilData == "Disediakan oleh pengurus" -> R.string.pilihan_Makan2
+                else -> R.string.pilihan_Makan2
+            }
+        } else {
+            R.string.pilihan_Makan2
+        }
+        return getString(stringRes)
     }
 
     private fun getPilihan(pilihan: String, isCat: Boolean, isDog: Boolean): String {
